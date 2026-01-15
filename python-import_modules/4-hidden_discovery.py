7973d6c3308e4e46d7c3edf4f336d90e885a4f63
Thorgalix <lucaspodevin@sfr.fr>
2026-01-15 15:09:36 +0100

    Edit: task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-import marshal, types
+import hidden_4
 
 if __name__ == "__main__":
-    with open("/tmp/hidden_4.pyc", "rb") as f:
-        f.read(16)   # ignorer l’entête du .pyc (magic number + timestamp)
-        code = marshal.load(f)   # charger le code compilé
-    module = types.ModuleType("hidden_4")  # créer un module vide
-    exec(code, module.__dict__)            # exécuter le code dans ce module
-
-    # récupérer les noms
-    names = [n for n in dir(module) if not n.startswith("__")]
-    for name in sorted(names):
-        print(name)
+    for name in sorted(dir(hidden_4)):
+        if not name.startswith("__"):
+            print(name)
