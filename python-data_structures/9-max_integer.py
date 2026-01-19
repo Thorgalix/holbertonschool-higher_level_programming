355732a094c01f6ea73be41da20b0042fdba06aa
Thorgalix <lucaspodevin@sfr.fr>
2026-01-19 14:01:17 +0100

    Edit: pycodestyle whitespace l5

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    val_max= my_list[0]
+    val_max = my_list[0]
     for num in my_list[1:]:
         if num > val_max:
             val_max = num
