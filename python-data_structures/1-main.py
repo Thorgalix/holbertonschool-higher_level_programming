d17d71c14201d140f0c1c0b4f07597f882fe01af
Thorgalix <lucaspodevin@sfr.fr>
2026-01-19 15:39:21 +0100

    P1T7: premier essai

--- python-data_structures/1-main.py
+++ python-data_structures/1-main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-delete_at = __import__('11-delete_at').delete_at
+add_tuple = __import__('7-add_tuple').add_tuple
 
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
