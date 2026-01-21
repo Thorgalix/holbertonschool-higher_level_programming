2e56995a97593261320beede702121caad057b44
Thorgalix <lucaspodevin@sfr.fr>
2026-01-21 12:04:02 +0100

    P4T0: premier essai

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module 0-add_integer:
+Contient une fonction qui additionne 2 entiers.
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Additionne deux entiers et retourne le résultat.
+
+    a et b doivent être des entiers ou des flottants ; les flottants
+    sont convertis en int.
+    Lève TypeError si a ou b ne sont pas des entiers ou flottants.
+
+    Retourne :
+        int : la somme de a et b
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
+    return a + b
