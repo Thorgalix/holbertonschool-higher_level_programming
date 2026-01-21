4a5fc135c40aaffd8aa4a7c65400cb37d8942b9f
Thorgalix <lucaspodevin@sfr.fr>
2026-01-21 14:08:28 +0100

    Edit: problème d'espace s'il n'y a que le prénom

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -19,5 +19,5 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    name = first_name + (" " + last_name if last_name else "")
-    print(f"My name is {name}", end="\n")
+    name = first_name + " " + (last_name if last_name else "")
+    print(f"My name is {name}")
