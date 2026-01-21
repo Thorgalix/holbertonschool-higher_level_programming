953a1b7171151d72b66c7efaa505766ca63a436d
Thorgalix <lucaspodevin@sfr.fr>
2026-01-21 14:00:29 +0100

    Edit: ajout de la doc

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+"""
+Module 3-say_my_name: Contient une fonction qui affiche
+le nom complet d'une personne.
+"""
+
 
 def say_my_name(first_name, last_name=""):
+    """Affiche le nom complet d'une personne.
+    Args:
+        first_name (str): le prénom de la personne
+        last_name (str, optional): le nom de famille de la personne.
+        Par défaut, une chaîne vide.
+    Raises:
+        TypeError: si first_name n'est pas une chaîne
+        TypeError: si last_name n'est pas une chaîne
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
