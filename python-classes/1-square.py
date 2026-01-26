31b93d07d425ee107d21a2b1d56e295742714ca3
Thorgalix <lucaspodevin@sfr.fr>
2026-01-26 10:38:52 +0100

    P1T1: premier essai

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
+class Square:  # Classe
+    """Class that defines a square."""
+    def __init__(self, size):  # Méthode
+        """Initializes a square with a given size.
+
+        Args:
+            size: The size of the square (no type or value verification).
+        """
+        self.__size = size  # Attribut
