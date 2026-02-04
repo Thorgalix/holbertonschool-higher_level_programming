786ece5873a02b0b5965be04a28a43bb0ae573c6
Thorgalix <lucaspodevin@sfr.fr>
2026-02-04 09:27:16 +0100

    P1T4: premier essai

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Function that checks if an object is an instance of a
+subclass of a class."""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if obj is an instance of a class that
+    inherited (directly or indirectly)
+    from a_class; otherwise False.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare with.
+
+    Returns:
+        bool: True if obj is an instance of a subclass
+        of a_class, False otherwise.
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
