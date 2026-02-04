cad60be852498a8ded8d68067ac9c72796adbed2
Thorgalix <lucaspodevin@sfr.fr>
2026-02-04 16:08:20 +0100

    Edit: ajout des tests pour tache 3

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -52,3 +52,17 @@ class CountedIterator:
         """
         self.count += 1
         return next(self.iterator)
+
+
+if __name__ == "__main__":
+    data = [1, 2, 3, 4]
+    counted_iter = CountedIterator(data)
+
+    try:
+        while True:
+            item = next(counted_iter)
+            print(
+                f"Got {item}, total {counted_iter.get_count()} items iterated."
+                )
+    except StopIteration:
+        print("No more items.")
