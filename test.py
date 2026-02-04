d97c037ebed39db1b4007ea4503e3db6a7f6af8b
Thorgalix <lucaspodevin@sfr.fr>
2026-02-04 08:50:31 +0100

    P1T0: premier essai

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-fruits1 = {'pomme' : 1.50, 'ananas' : 2.20}
-fruits2 = {'orange' : 2.00, 'kiwi' : 5.20}
-print (fruits1.get("pomme"), "avant")
-print (fruits1.get("ananas"), "avant")
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+        
+    def display_information(self):
+        print(f"Car : {self.brand} {self.model}")
+        
+# Objects création
+car1 = Car("Toyota", "Corolla")
+car2 = Car("Honda", "Civic")
 
-fruits1["pomme"] = 2.10
-fruits1["ananas"] = 3.50
-
-print (fruits1.get("pomme"), "après")
-print (fruits1.get("ananas"), "après")
+# Method usage
+car1.display_information()
+car2.display_information()
 
 
