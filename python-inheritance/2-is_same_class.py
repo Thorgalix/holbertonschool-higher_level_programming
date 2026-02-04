39453e1137a329931151ca975055f1272574213f
Thorgalix <lucaspodevin@sfr.fr>
2026-02-04 09:23:33 +0100

    Edit: pycodestyle tache 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Function that checks if an object is exactly an instance of a specified class."""
+"""Function that checks if an object is exactly an instance of
+a specified class."""
 
 
 def is_same_class(obj, a_class):
