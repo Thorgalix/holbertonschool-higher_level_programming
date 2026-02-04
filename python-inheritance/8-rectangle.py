aa4fdc47f76d770a4b85fea961a1a2b758d4a0b5
Thorgalix <lucaspodevin@sfr.fr>
2026-02-04 09:45:45 +0100

    Edit: pycodestyle tache 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Module that defines a Rectangle class that inherits from BaseGeometry."""
+"""Module that defines a Rectangle class that inherits
+from BaseGeometry."""
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     """Represents a rectangle with validated width and height."""
