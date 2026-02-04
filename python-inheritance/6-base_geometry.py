9ac3047733a418c27814a9364e6cf928c4e3eb25
Thorgalix <lucaspodevin@sfr.fr>
2026-02-04 09:31:24 +0100

    P1T6: premier essai

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that defines BaseGeometry class with an area method."""
+
+
+class BaseGeometry:
+    """Class BaseGeometry with a placeholder area method."""
+    def area(self):
+        """Raises an exception because area is not implemented."""
+        raise Exception("area() is not implemented")
