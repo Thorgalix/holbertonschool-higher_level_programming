103aa7b20857e007cb02fa975010ccbee88ad250
Thorgalix <lucaspodevin@sfr.fr>
2026-02-09 13:25:35 +0100

    Edit: pycodestyle docstrings 8-classe_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+"""
+Module that provides a function to retrieve the dictionary
+representation of a class instance for JSON serialization.
+"""
+
 
 def class_to_json(obj):
+    """
+    Returns the dictionary representation of a class instance's
+    attributes for JSON serialization.
+
+    Args:
+        obj (any): Instance of a class whose attributes are to be
+                   retrieved. All attributes should be of type
+                   list, dict, str, int, or bool.
+
+    Returns:
+        dict: Dictionary containing all instance attributes with
+              their names as keys and their values as values.
+    """
     return obj.__dict__
