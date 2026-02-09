b613b6bae66e4a591714b8a516fc884b59b7899b
Thorgalix <lucaspodevin@sfr.fr>
2026-02-09 10:31:52 +0100

    Add: docstrings 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+Module for reading and printing the content of a text file.
+"""
+
 
 def read_file(filename=""):
+    """
+    Reads a text file (UTF-8) and prints its content to stdout.
+
+    Args:
+        filename (str): Path to the file to be read.
+    """
     with open(filename, encoding='utf-8') as f:
         print(f.read())
