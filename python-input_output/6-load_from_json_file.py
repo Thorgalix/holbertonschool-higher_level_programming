3a687f37a531e8aee8e653ce702177de62e860ee
Thorgalix <lucaspodevin@sfr.fr>
2026-02-09 11:18:57 +0100

    P1T6: premier essai

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for loading Python objects from a JSON file.
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Creates a Python object from a JSON file.
+
+    Args:
+        filename (str): Path to the JSON file to read.
+
+    Returns:
+        any: Python object deserialized from the JSON file.
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
