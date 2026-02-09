7721f51b2de037404f386bd3859037a436f77e7e
Thorgalix <lucaspodevin@sfr.fr>
2026-02-09 10:43:59 +0100

    P1T1: premier essai

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module for writing text to a file.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF-8) and returns the number of
+    characters written.
+
+    Args:
+        filename (str): Path to the file to write to.
+        text (str): Text to be written to the file.
+
+    Returns:
+        int: Number of characters written.
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
