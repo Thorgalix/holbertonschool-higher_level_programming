5c16d81b53e9591d741f0f0e4765464d4a8a293c
Thorgalix <lucaspodevin@sfr.fr>
2026-02-16 17:03:44 +0100

    Ajout de la gestion des requêtes GET pour les endpoints /status, /info et /data dans le serveur HTTP

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -2,15 +2,42 @@
 """Task 02 - RESTful API - Python Requests"""
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import socketserver
+import json
 
 PORT = 8000
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         def do_GET(self):
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write(b"Hello, this is a simple API!")
+            data1 = {"name": "John", "age": 30, "city": "New York"}
+            data2 = {"version": "1.0", "description": "A simple API built with http.server"}
+            if self.path == "/status":
+                self.send_response(200)
+                self.send_header("Content-type", "text/plain")
+                self.end_headers()
+                self.wfile.write(b"OK")
+            elif self.path == "/info":
+                datas2 = json.dumps(data2)
+                self.send_response(200)
+                self.send_header("Content-type", "application/json")
+                self.end_headers()
+                self.wfile.write(datas2.encode())
+            elif self.path == "/data":
+                datas1 = json.dumps(data1)
+                self.send_response(200)
+                self.send_header("Content-type", "application/json")
+                self.end_headers()
+                self.wfile.write(datas1.encode())
+            else:
+                if self.path == "/":
+                    self.send_response(200)
+                    self.send_header("Content-type", "text/plain")
+                    self.end_headers()
+                    self.wfile.write(b"Hello, this is a simple API!")
+                else:
+                    self.send_response(404)
+                    self.send_header("Content-type", "text/plain")
+                    self.end_headers()
+                    self.wfile.write(b"Endpoint not found")
 
 serv_adress = ("", PORT)
 httpd = HTTPServer(serv_adress, SimpleHTTPRequestHandler)
