0ac1ce39505dbc83af2ae3073accb77e412669c3
Thorgalix <lucaspodevin@sfr.fr>
2026-03-03 09:43:08 +0100

    Ajout d'un script SQL pour créer une base de données et un utilisateur avec des privilèges de sélection

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- Script pour créer une database avec un user_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
