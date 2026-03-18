5d858386b45beb6e56a797e1e179558305500f5f
Thorgalix <lucaspodevin@sfr.fr>
2026-03-18 11:44:55 +0100

    Ajout d'un script pour afficher "JavaScript is amazing" et d'un fichier README pour le projet

---
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/** A script that prints “JavaScript is amazing”
+* Use console.log() to print the string
+* You must use an array and a loop to print the string
+*/
+
+const str = "JavaScript is amazing";
+console.log(str);
