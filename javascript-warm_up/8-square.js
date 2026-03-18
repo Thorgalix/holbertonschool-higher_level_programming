47a8d4e54a5fa52c56d9fca9da860c9ccd4968c6
Thorgalix <lucaspodevin@sfr.fr>
2026-03-18 15:18:23 +0100

    Ajout du script 8-square.js pour afficher un carré de 'X' d'une taille spécifiée

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const x = Number(process.argv[2]);
+
+if (isNaN(x) || !Number.isInteger(x)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('X'.repeat(x));
+  }
+}
