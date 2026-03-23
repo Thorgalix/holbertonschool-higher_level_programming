784fdf1967b49bafcf5b6c45b2d51ea6a48ef85a
Thorgalix <lucaspodevin@sfr.fr>
2026-03-23 13:54:58 +0100

    Ajout du script 6-script.js pour récupérer et afficher le nom d'un personnage Star Wars dans le div "character" et mise à jour de 0-main.html pour référencer le nouveau script.

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,7 @@
+const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const character = document.querySelector('#character');
+    character.textContent = data.name;
+  });
